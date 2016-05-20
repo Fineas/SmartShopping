76922c41f61c84550821e086c322b7de49ea5d19
Senibelan <malessebastian@gmail.com>
2016-05-20 23:32:35 +0300

    Magazin modelform

--- smartshopping/magazin/models.py
+++ smartshopping/magazin/models.py
@@ -1,8 +1,18 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django import forms
+
 
 class LantMagazine(models.Model):
     nume = models.CharField(max_length=100, null=True)
     email = models.EmailField(null=True)
     cod = models.CharField(max_length=100, null=True)
+
+class LantMagazineForm(forms.ModelForm):
+
+
+    class Meta: 
+        model = LantMagazine
+        fields = ['nume', 'email', 'cod']
+
