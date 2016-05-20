898e4c344eca7938cc25d98e1949843b11ef9213
Senibelan <malessebastian@gmail.com>
2016-05-21 01:51:53 +0300

    oferte app

---
+++ smartshopping/oferte/models.py
@@ -0,0 +1,13 @@
+from __future__ import unicode_literals
+
+from django.db import models
+from magazin.models import Magazin
+
+class Oferta(models.Model):
+    magazin = models.ForeignKey(Magazin, related_name="oferte", null=True)
+    titlu = models.CharField(max_length=100, null=True)
+    descriere = models.CharField(max_length=300, null=True)
+    data_inceperii = models.DateTimeField(null=True)
+    data_sfarsirii = models.DateTimeField(null=True)
+
+
