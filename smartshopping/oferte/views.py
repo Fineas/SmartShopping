8bae65fcace35e551798edb1854d0daf0a0d2b33
Senibelan <malessebastian@gmail.com>
2016-05-21 02:35:34 +0300

    Oferta view

--- smartshopping/oferte/views.py
+++ smartshopping/oferte/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import OfertaForm
+
+
+def oferta(request):
+    oferta_form = OfertaForm(data=request.POST or None)
+    if request.method == 'POST':
+        if oferta_form.is_valid():
+            instance = oferta_form.save(commit=False)
+            instance.save()
+    return render(request, 'magazin/lant.html', {
+        'form': oferta_form
+    })
