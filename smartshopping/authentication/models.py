db55f37897e9e661e875dc33a4771484717ace44
Senibelan <malessebastian@gmail.com>
2016-05-21 16:15:52 +0300

    Auth refact

--- smartshopping/authentication/models.py
+++ smartshopping/authentication/models.py
@@ -1,7 +1,8 @@
 from __future__ import unicode_literals
+
+from django import forms
 from django.contrib.auth.models import User
 from django.db import models
-
 from django.core.urlresolvers import reverse
 
 
@@ -12,4 +13,20 @@ class Account(models.Model):
     city = models.CharField(max_length=100, null=True)
     country = models.CharField(max_length=36, null=True)
     address = models.CharField(max_length=100, null=True)
+
+
+class LoginForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ['username', 'password']
+    
+    def clean_password(self): 
+        username = self.cleaned_data['username']
+        password = self.cleaned_data['password']
+        user = authenticate(username=username,password=password)
+        if user is None:
+            raise forms.ValidationError("Incorrect username or password")
+        return password
+        
+    
 
--- smartshopping/authentication/urls.py
+++ smartshopping/authentication/urls.py
@@ -3,8 +3,5 @@ from . import views
 
 urlpatterns = [
     url(r'^logout/$', views.logout_view, name='logout'),
-    url(r'^register/', views.register_page, name='register'),
     url(r'^login/', views.login_page, name='login'),
-    url(r'^reset/', views.reset_pass, name='reset'),
-
 ]
