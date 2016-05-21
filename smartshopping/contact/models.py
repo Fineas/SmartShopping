8e54f5c8e4aa411c917b6dd3ae88aa93fd5706b6
Emanuel Covaci <covaciemanuel98@gmail.com>
2016-05-22 02:54:24 +0300

    contact not work

--- smartshopping/contact/models.py
+++ smartshopping/contact/models.py
@@ -3,3 +3,9 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+
+class Contact(models.Model):
+    frist_name = models.CharField(max_length=20)
+    phone = models.IntegerField(null=True)
+    message = models.CharField(max_length=1000, null=True)
